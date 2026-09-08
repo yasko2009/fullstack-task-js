ac8a334aa8820c4b16db7bb6b0c8536a61a545ff
Danya07 <kuzychenko2007@gmail.com>
2026-09-08 23:42:27 +0300

    Handle invalide JWT and redirect to login

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -4,6 +4,7 @@ const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
 });
 
+// Добавляем JWT ко всем запросам
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
 
@@ -13,5 +14,18 @@ api.interceptors.request.use((config) => {
 
   return config;
 });
+
+// Если Backend вернул 401 — удаляем токен и отправляем на login
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      localStorage.removeItem("token");
+      window.location.href = "/login";
+    }
+
+    return Promise.reject(error);
+  }
+);
 
 export default api;
